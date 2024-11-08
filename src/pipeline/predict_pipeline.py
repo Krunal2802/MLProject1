3ca6095c36cff3dcb96b779cc39a6c2111e7817c
Krunal2802 <krunals27920@gmail.com>
2024-11-09 00:06:55 +0530

    complete project with frontend

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -10,6 +10,21 @@ class PredictPipeline:
     def __init__(self):
         pass
 
+    def predict(self,features):
+        try:
+            model_path = 'artifacts\model.pkl'
+            preprocessor_path = 'artifacts\preprocessor.pkl'
+            model = load_object(file_path= model_path)
+            preprocessor = load_object(file_path= preprocessor_path)
+
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+
+        except Exception as e:
+            raise customException(e,sys)
+        
+        return preds
+
 class CustomData:
     def __init__(self,
         gender: str,
@@ -27,3 +42,21 @@ class CustomData:
         self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
         self.writing_score = writing_score
+
+    # self means that object which calls this method
+    def get_data_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+        
+        except Exception as e:
+            raise customException(e,sys)
